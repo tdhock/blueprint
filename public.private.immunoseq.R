57770ec6f1d25026e98ddcac9787b5df7591809d
Toby Dylan Hocking <tdhock5@gmail.com>
2015-09-03 12:42:10 -0400

    ID matching

--- public.private.immunoseq.R
+++ public.private.immunoseq.R
@@ -19,3 +19,38 @@ public.private.immunoseq <-
   has.immunoseq[public.private, ][order(immunoseqID), ]
 
 write.csv(public.private.immunoseq, "public.private.immunoseq.csv")
+
+ihec <- fread("ihec-all-histone.txt")
+setnames(ihec, sub(" *$", "", names(ihec)))
+##No matches:
+ihec[Donor %in% public.with.immunoseq$public, ]
+ihec[Sample %in% has.immunoseq$private, ]
+
+## For BluePrint samples that were analyzed at McGill, they were given
+## different Sample IDs on the portal. For example, MS029801 is the
+## same as the BluePrint sample with private ID S002EV.
+(ihec.bp <- ihec[Sample %in% public.with.immunoseq$public, ])
+setkey(public.private, public)
+ihec.bp$private <- public.private[ihec.bp$Sample, private]
+ihec.bp
+
+## For BluePrint samples that were processed at Cambridge, they kept
+## their "private" IDs on the portal as Donor. But they do have
+## "public" IDs according to David.
+(ihec.mcgill <- ihec[Donor %in% has.immunoseq$private &
+     grepl("H3K27ac", `Track Name`), ])
+setkey(public.private, private)
+ihec.mcgill$public <- public.private[ihec.mcgill$Donor, public]
+ihec.mcgill
+
+## Seems like there are no samples that have been analyzed by both
+## BluePrint/Cambridge and by McGill.
+ihec[Sample %in% ihec.mcgill$public, ]
+ihec[Donor %in% ihec.mcgill$public, ]
+ihec[Sample %in% ihec.bp$private, ]
+ihec[Donor %in% ihec.bp$private, ]
+
+## This is a subset of the IHEC samples:
+edcc <- fread("edcc-CD4positive-alpha-beta-T-cell.txt")
+setnames(edcc, sub(" *$", "", names(edcc)))
+edcc[Sample %in% public.with.immunoseq$public, ][order(Donor), ]
